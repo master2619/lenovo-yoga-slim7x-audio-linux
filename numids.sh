d92fd3cf3ca0e32ce27856e65e32e7f4f7321a53
Deepesh Singh Chauhan <95081449+master2619@users.noreply.github.com>
2026-04-09 11:58:50 +0530

    Update and rename test.sh to numids.sh

---
+++ numids.sh
@@ -0,0 +1,12 @@
+amixer -c 0 cset numid=103 255
+
+amixer -c 0 cset numid=73 2
+amixer -c 0 cset numid=74 2
+amixer -c 0 cset numid=90 2
+amixer -c 0 cset numid=91 2
+
+
+amixer -c 0 cset numid=78 3
+amixer -c 0 cset numid=82 4
+amixer -c 0 cset numid=95 3
+amixer -c 0 cset numid=99 4
